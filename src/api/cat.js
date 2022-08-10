d1843be8ec63171e39c1b0949dc51c97c381ff65
Nicolai Friis <nfriis24@gmail.com>
2022-08-10 09:59:18 +0200

    Day 3
    Added error handling for route "/api/cat".
    Added a utility function to handle data in the body of a request.
    Fixed SendFile and StreamFile functions.

--- src/api/cat.js
+++ src/api/cat.js
@@ -1,10 +1,76 @@
-const { sendJSON } = require("../utilities")
+const { sendJSON, getData } = require("../utilities")
+const currentRoute = "/api/cat";
 
 module.exports = {
     "GET": {
-        handler: function(req, res, param) {
+        handler: function (req, res, param) {
+            let json = {}
+
+            if (param) {
+                json = {
+                    route: currentRoute,
+                    method: req.method,
+                    says: "Miauw",
+                    param: param
+                }
+            }
+            else {
+                json = [{
+                    route: currentRoute,
+                    method: req.method,
+                    says: "Miauw1",
+                    param: param
+                },
+                {
+                    route: currentRoute,
+                    method: req.method,
+                    says: "Miauw2",
+                    param: param
+                }]
+            }
+
+            sendJSON(req, res, json);
+        }
+    },
+    "POST": {
+        handler: function (req, res, param) {
+            if (param) {
+                sendJSON(req, res, {route: currentRoute, method: req.method, error: "Parameter not allowed"}, 405)
+                return;
+            };
+
+            getData(req)
+                .then((input) => {
+                    sendJSON(req, res, { route: currentRoute, method: req.method, says: "Miauw", body: input })
+                });
+        }
+    },
+    "PUT": {
+        handler: function (req, res, param) {
+            if (!param) {
+                sendJSON(req, res, {route: currentRoute, method: req.method, error: "Parameter required"}, 405)
+                return;
+            };
+            
             const json = {
-                route: "/api/cat",
+                route: currentRoute,
+                method: req.method,
+                says: "Miauw",
+                param: param
+            }
+
+            sendJSON(req, res, json);
+        }
+    },
+    "DELETE": {
+        handler: function (req, res, param) {
+            if (!param) {
+                sendJSON(req, res, {route: currentRoute, method: req.method, error: "Parameter required"}, 405)
+                return;
+            };
+            
+            const json = {
+                route: currentRoute,
                 method: req.method,
                 says: "Miauw",
                 param: param
--- src/utilities.js
+++ src/utilities.js
@@ -1,6 +1,8 @@
 const {readFile, createReadStream} = require("fs");
 const { extname } = require("path");
+const { resolve } = require("path/posix");
 const { hrtime } = require("process");
+const mimetypes = require("./mimetypes")
 
 exports.sendText = function(req, res, msg, status = 200) {
     res.statusCode = status;
@@ -15,30 +17,31 @@ exports.sendJSON = function(req, res, obj, status = 200) {
 }
 
 exports.sendFile = function(req, res, filename) {
-    readFile(filename, function(err, filecontent) {
-        if(err) {
-            exports.sendJSON(req, res, {"error": err.message}, 404);
-            return;
-        }
-
-        res.statusCode = 200;
-        // res.setHeader("Content-type", "text/html");
-        res.end(filecontent);
-    })
+	const extension = extname(filename)
+	const mimetype = mimetypes[extension].type
+	readFile(filename, function(err, filecontent) {
+		if (err) {
+			exports.sendJSON(req, res, {"error": err.message}, 404)
+			return
+		}
+		res.statusCode = 200
+		res.setHeader("Content-type", mimetype)
+		res.end(filecontent)
+	})
 }
 
 exports.streamFile = function(req, res, filename) {
-    const ext = extname(filename);
-    const type = mimetypes[ext].type;
-    const stream = createReadStream(filename);
-    stream.on("error", function(err) {
-        console.log(err);
-        exports.sendJSON(req, res, {error: {msg: "Something went wront"}}, 404);
-        return;
-    });
-    res.statusCode = 200;
-    res.setHeader("Content-type",type);
-    stream.pipe(res);
+	const extension = extname(filename)
+	const mimetype = mimetypes[extension].type
+	const stream = createReadStream(filename)
+	stream.on("error", function(err) {
+		console.log(err)
+		exports.sendJSON(req, res, {error: {msg: "Something went wrong"}}, 404)
+		return
+	})
+	res.statusCode = 200
+	res.setHeader("Content-type", mimetype)
+	stream.pipe(res)
 }
 
 exports.redirect = function(res, url) {
@@ -55,4 +58,18 @@ exports.logger = function(req, res) {
         logStr += ` [${res.statusCode} ${res.statusMessage}]  -  ${duration}ms`;
         console.log(logStr);
     })
+}
+
+exports.getData = function (req) {
+    return new Promise((resolve, reject) => {
+        let dataStr = "";
+
+        req.on("data", function (chunk) {
+            dataStr += chunk;
+        })
+        req.on("end", function () {
+            console.log(JSON.parse(dataStr));
+            resolve(JSON.parse(dataStr));
+        });
+    });
 }
